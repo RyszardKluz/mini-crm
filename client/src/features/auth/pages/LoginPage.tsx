3d8159873e0456cc2e459ffbe9701804f10fb97b
Ryszard Kluz <Ryszard.kluz.fm@gmail.com>
2025-11-18 20:16:35 +0100

    feature(client-auth): adjust LoginPage, and RegisterPage components to use showToast logic

--- client/src/features/auth/pages/LoginPage.tsx
+++ client/src/features/auth/pages/LoginPage.tsx
@@ -2,17 +2,19 @@ import { useNavigate } from 'react-router-dom';
 import { LoginForm } from '../components/LoginForm';
 import type { LoginFormData } from '../schemas/loginSchema';
 import { useAuthStore } from '../store/authStore';
+import { useToast } from '../../../hooks/useToast';
 
 export const LoginPage = () => {
   const { login } = useAuthStore();
   const navigate = useNavigate();
+  const { showToast } = useToast();
 
   const loginHandler = async ({ email, password }: LoginFormData) => {
     try {
       await login(email, password);
       navigate('/dashboard');
     } catch (error) {
-      console.log((error as Error).message);
+      showToast((error as Error).message, 'error');
     }
   };
 
--- client/src/features/auth/pages/RegisterPage.tsx
+++ client/src/features/auth/pages/RegisterPage.tsx
@@ -1,31 +1,33 @@
 import { useNavigate } from 'react-router-dom';
 import { RegisterForm } from '../components/RegisterForm';
 import type { RegisterFormData } from '../schemas/registerSchema';
-
-const registerHandler = async (data: RegisterFormData) => {
-  const dataTransfer = {
-    password: data.password,
-    name: data.name,
-    email: data.email,
-  };
-  try {
-    const response = await fetch('http://localhost:5000/auth/register', {
-      method: 'POST',
-      body: JSON.stringify(dataTransfer),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    const data = await response.json();
-    if (!response.ok) {
-      throw new Error(data.message);
-    }
-    console.log('User created', data);
-  } catch (error) {
-    console.log((error as Error).message);
-  }
-};
+import { useToast } from '../../../hooks/useToast';
 
 export const RegisterPage = () => {
+  const { showToast } = useToast();
+  const registerHandler = async (data: RegisterFormData) => {
+    const dataTransfer = {
+      password: data.password,
+      name: data.name,
+      email: data.email,
+    };
+    try {
+      const response = await fetch('http://localhost:5000/auth/register', {
+        method: 'POST',
+        body: JSON.stringify(dataTransfer),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+      console.log('User created', data);
+    } catch (error) {
+      showToast((error as Error).message, 'error');
+    }
+  };
+
   const navigate = useNavigate();
 
   const handleFormSubmit = (data: RegisterFormData) => {
