c43f9e99d15c6c8120f82edc7976319d7e0014c2
Ryszard Kluz <Ryszard.kluz.fm@gmail.com>
2025-11-18 20:03:48 +0100

    feature(client-auth): -refactor LoginForm to use react-hook-form with zod validation -add layout, and login logic to LoginPage placeholder

--- client/src/features/auth/components/LoginForm.tsx
+++ client/src/features/auth/components/LoginForm.tsx
@@ -1,42 +1,59 @@
 import { useNavigate } from 'react-router-dom';
-export const LoginForm = () => {
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { loginSchema, type LoginFormData } from '../schemas/loginSchema';
+
+type LoginFormProps = {
+  onFormSubmit: (data: LoginFormData) => void;
+  onNavigateToRegister: () => void;
+};
+
+export const LoginForm = ({
+  onFormSubmit,
+  onNavigateToRegister,
+}: LoginFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>({ resolver: zodResolver(loginSchema) });
+
   const navigate = useNavigate();
   return (
-    <div className='flex flex-col items-center justify-center  bg-slate-400'>
+    <div className='flex flex-col items-center justify-center  bg-slate-400 text-sm'>
       <form
+        onSubmit={handleSubmit(onFormSubmit)}
         action=''
         method='post'
         className='flex flex-col gap-3 bg-white p-6 rounded-lg shadow-md w-80'
       >
         <input
           type='text'
-          name='email'
           className='p-2 border rounded-md'
           placeholder='email@email.com'
+          {...register('email')}
         />
+        {errors.email && <p>{errors.email.message}</p>}
+
         <input
           type='password'
-          name='password'
           className='p-2 border rounded-md'
           placeholder='***********'
+          {...register('password')}
         />
+        {errors.password && <p>{errors.password.message}</p>}
 
         <div className='flex justify-between items-center text-sm'>
           <button
             type='button'
             className='text-blue-600 hover:underline'
-            onClick={() => {
-              navigate('/forgot-password');
-            }}
+            onClick={onNavigateToRegister}
           >
             Forgot password?
           </button>
           <button
             type='submit'
             className='bg-slate-700 text-white px-4 py-1 rounded-md hover:bg-slate-800'
-            onClick={(e) => {
-              e.preventDefault();
-            }}
           >
             Log in
           </button>
--- client/src/features/auth/pages/LoginPage.tsx
+++ client/src/features/auth/pages/LoginPage.tsx
@@ -1,5 +1,30 @@
+import { useNavigate } from 'react-router-dom';
 import { LoginForm } from '../components/LoginForm';
+import type { LoginFormData } from '../schemas/loginSchema';
+import { useAuthStore } from '../store/authStore';
 
 export const LoginPage = () => {
-  return <LoginForm />;
+  const { login } = useAuthStore();
+  const navigate = useNavigate();
+
+  const loginHandler = async ({ email, password }: LoginFormData) => {
+    try {
+      await login(email, password);
+      navigate('/dashboard');
+    } catch (error) {
+      console.log((error as Error).message);
+    }
+  };
+
+  const handleNavigateToRegister = () => navigate('/register');
+  const handleFormSubmit = (data: LoginFormData) => {
+    loginHandler(data);
+  };
+
+  return (
+    <LoginForm
+      onFormSubmit={handleFormSubmit}
+      onNavigateToRegister={handleNavigateToRegister}
+    />
+  );
 };
