9ac0c631329c8ff59b076785beb239b947b4c54f
Ryszard Kluz <Ryszard.kluz.fm@gmail.com>
2025-11-18 20:05:05 +0100

    feat(client-layout): add Toast, and Spinner component in AuthLayout component.

--- client/src/features/auth/components/AuthLayout.tsx
+++ client/src/features/auth/components/AuthLayout.tsx
@@ -1,8 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { Spinner } from '../../../components/Spinner';
+import { Toast } from '../../../components/Toast';
+import { useSpinner } from '../hooks/useSpinner';
 
 export const AuthLayout = () => {
+  const loading = useSpinner();
+  if (loading) return <Spinner />;
   return (
     <div className='grid grid-cols-3 grid-col h-screen p-10 gap-1 bg-zinc-200 '>
+      <Toast />
       <div className='bg-slate-700 text-white flex items-start p-12 justify-center col-1 rounded-lg'>
         <h1 className='text-4xl font-bold'>Mini CRM!</h1>
       </div>
