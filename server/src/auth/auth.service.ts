8116dd316886449212378142496f770947216dd9
Ryszard Kluz <Ryszard.kluz.fm@gmail.com>
2025-11-23 20:39:11 +0100

    feature(server-auth-2): -add authorization method to auth routes,controllers and services -get rid off unneccessary error logic in auth.controller -add findById static method to user.repository

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -5,6 +5,10 @@ import { hashPassword, comparePassword } from '../core/utils/hashPassword.util';
 import { v4 as uuidV4 } from 'uuid';
 import { LoginDTO } from './login.dto';
 import { generateToken } from '../core/utils/jwt.util';
+import { AuthorizeDTO } from './authorize.dto';
+import { AuthorizeResponseDTO } from './authorizeResponse.dto';
+import { UserResponseDTO } from '../users/userResponse.dto';
+import { AppError } from '../core/errors/app-error';
 
 export class AuthenticationService {
   register = async (data: RegisterDTO): Promise<User> => {
@@ -19,21 +23,30 @@ export class AuthenticationService {
     };
     const exists = await UserRepository.findByEmail(data.email);
     if (exists) {
-      throw new Error('User with this email already exists!');
+      throw new AppError('User with this email already exists!', 400);
     }
     const createdUser = await UserRepository.createUser(dataTransfer);
     return createdUser;
   };
-  login = async (data: LoginDTO): Promise<string> => {
+  login = async (data: LoginDTO): Promise<AuthorizeResponseDTO> => {
     const existingUser = await UserRepository.login(data.email);
     if (!existingUser) {
-      throw new Error('No user with this email exists, please try again.');
+      throw new AppError('No user with this email exists, please try again.', 401);
     }
     const matchingPassword = await comparePassword(data.password, existingUser.password);
     if (!matchingPassword) {
-      throw new Error('Invalid password, please try again');
+      throw new AppError('Invalid password, please try again', 401);
     }
     const token = generateToken({ userId: existingUser.id });
-    return token;
+    const dataTransfer = { token, userId: existingUser.id, email: existingUser.email };
+    return dataTransfer;
+  };
+  authorize = async (data: AuthorizeDTO): Promise<UserResponseDTO> => {
+    const user = await UserRepository.findById(data.userId);
+    if (!user) {
+      throw new AppError('Authentication failure!', 500);
+    }
+    const dataTransfer = { userId: user.id, email: user.email };
+    return dataTransfer;
   };
 }
